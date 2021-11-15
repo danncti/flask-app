8aa5037dbd47b7d6a977e43849f6a4e0bc8bc525
Daniel <danncti@gmail.com>
2021-11-15 19:43:44 +0000

    working form

--- routes.py
+++ routes.py
@@ -9,7 +9,12 @@ def index():
     # return render_template('index.html', current_title = 'Custom Title')
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        print('Submitted title', form.title.data)
+        return render_template('about.html', form=form,
+                               title=form.title.data)
+
     return render_template('about.html', form=form)
