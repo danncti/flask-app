e842bcf30d42a1a979546207ab2307a66c39d5de
Daniel <danncti@gmail.com>
2021-11-16 23:17:52 +0000

    working task app

--- routes.py
+++ routes.py
@@ -41,7 +41,6 @@ def edit(task_id):
         if form.validate_on_submit():
             task.title = form.title.data
             task.date = datetime.utcnow()
-
             try:
                 db.session.commit()
                 flash('Task has been updated successfully')
@@ -50,10 +49,36 @@ def edit(task_id):
                 raise
             # finally:
             #     session.close()
-
-
             return redirect(url_for('index'))
 
         form.title.data = task.title
         return render_template('edit.html', form=form, task_id=task_id)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    task = Task.query.get(task_id)
+    form = forms.DeleteTaskForm()
+
+    if task:
+        if form.validate_on_submit():
+            task.date = datetime.utcnow()
+
+            try:
+                db.session.delete(task)
+                db.session.commit()
+                flash('Task has been deleted successfully')
+            except:
+                db.session.rollback()
+                raise
+            # finally:
+            #     session.close()
+
+            return redirect(url_for('index'))
+
+        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
+    else:
+        flash('Task not found')
     return redirect(url_for('index'))
